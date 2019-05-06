093cbf9ee1a0058072889f9e6429347220bcf90e
Jairo Honorio <registrofx@gmail.com>
2019-05-06 12:53:44 -0300

    Fix vibration crashes on some devices and code formatted.

--- android/GodotVibrate.java
+++ android/GodotVibrate.java
@@ -2,30 +2,35 @@ package org.godotengine.godot;
 
 //imports
 import android.app.Activity;
+import android.os.Build;
 import android.content.Intent;
 import android.os.Vibrator;
+import android.os.VibrationEffect;
 import android.content.Context;
 
 public class GodotVibrate extends Godot.SingletonBase {
 
 	Activity m_pActivity;
 
-    	public void doVibrate(int ms)
-	{
+	public void doVibrate(int ms) {
 		Vibrator v = (Vibrator) m_pActivity.getSystemService(Context.VIBRATOR_SERVICE);
- 		
+
 		// Vibrate for ms milliseconds
- 		v.vibrate(ms);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			v.vibrate(VibrationEffect.createOneShot(ms, VibrationEffect.DEFAULT_AMPLITUDE));
+		} else {
+			v.vibrate(ms);
+		}
 	}
 
 	static public Godot.SingletonBase initialize(Activity p_activity) {
 		return new GodotVibrate(p_activity);
-    	}
+	}
 
-    	public GodotVibrate(Activity p_activity) {
+	public GodotVibrate(Activity p_activity) {
 		m_pActivity = p_activity;
 
-		//register class name and functions to bind
-		registerClass("GodotVibrate", new String[]{"doVibrate"});
-    	}
+		// register class name and functions to bind
+		registerClass("GodotVibrate", new String[] { "doVibrate" });
+	}
 }
