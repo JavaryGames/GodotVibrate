1484f6658fd0ab07692ffb2c145f9bfd81b5653f
Pedro Ciambra <pedrociambra@gmail.com>
2019-06-07 18:24:07 -0300

    Run vibrate function on a thread

--- android/GodotVibrate.java
+++ android/GodotVibrate.java
@@ -8,28 +8,39 @@ import android.os.Vibrator;
 import android.os.VibrationEffect;
 import android.content.Context;
 import android.util.Log;
+import android.os.AsyncTask;
+
 
 public class GodotVibrate extends Godot.SingletonBase {
 
 	Activity m_pActivity;
 
-	public void doVibrate(int ms) {
-		Vibrator v = (Vibrator) m_pActivity.getSystemService(Context.VIBRATOR_SERVICE);
+	public void doVibrate(final int ms) {
 
-		if (v == null || !v.hasVibrator()) {
-			return;
-		}
 
-		try {
-			// Vibrate for ms milliseconds
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-				v.vibrate(VibrationEffect.createOneShot(ms, VibrationEffect.DEFAULT_AMPLITUDE));
-			} else {
-				v.vibrate(ms);
+		AsyncTask.execute(new Runnable() {
+			@Override
+			public void run() {
+				Vibrator v = (Vibrator) m_pActivity.getSystemService(Context.VIBRATOR_SERVICE);
+
+				if (v == null || !v.hasVibrator()) {
+					return;
+				}
+		
+				try {
+					// Vibrate for ms milliseconds
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+						v.vibrate(VibrationEffect.createOneShot(ms, VibrationEffect.DEFAULT_AMPLITUDE));
+					} else {
+						v.vibrate(ms);
+					}
+				} catch (NullPointerException e) {
+					Log.d("godot", "Error while trying to make the device vibrate (known issue in Samsung Oreo (8.1.0) devices).");
+				}
 			}
-		} catch (NullPointerException e) {
-			Log.d("godot", "Error while trying to make the device vibrate (known issue in Samsung Oreo (8.1.0) devices).");
-		}
+		 });
+
+
 	}
 
 	static public Godot.SingletonBase initialize(Activity p_activity) {
